b0e60c2074555e7c6a7adb94d81e61105cb6f137
Glymph-PHS <peterhusted@hotmail.com>
2021-08-21 03:01:47 +0200

    fix endlessh API and new colors :)

--- cool_api/urls.py
+++ cool_api/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'ftp-login', views.FtpLoginViewSet)
-router.register(r'endlessh', views.EndlesshViewSet)
+router.register(r'endlessh-login', views.EndlesshViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('ftp-login/', include('rest_framework.urls', namespace='rest_framework_ftp-login')),
-    path('endlessh/', include('rest_framework.urls', namespace='rest_framework_endlessh')),
+    path('endlessh-login/', include('rest_framework.urls', namespace='rest_framework_endlessh')),
 ]
