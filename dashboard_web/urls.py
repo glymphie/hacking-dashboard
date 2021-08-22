967a901e86b363f7efbd61a4fb21ec9593a92504
0xUnicorn <17219001+0xUnicorn@users.noreply.github.com>
2021-08-22 23:49:01 +0100

    Added new menu link 'apps' and two new sites for 'ftp' and 'endlessh' overviews.

--- dashboard_web/urls.py
+++ dashboard_web/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard', )
+    path('', views.dashboard, name='dashboard'),
+    path('ftp', views.ftp, name='ftp_overview'),
+    path('endlessh', views.endlessh, name='endlessh_overview')
+
 ]
--- dashboard_web/views.py
+++ dashboard_web/views.py
@@ -8,6 +8,14 @@ from cool_api.models import FtpLogin, Endlessh
 
 def dashboard(request):
     ftp_logins = FtpLogin.get_10_latest()
-    endlessh = Endlessh.get_all()
+    endlessh = Endlessh.get_10_latest()
     context = {'ftplogins' : ftp_logins, 'endlessh': endlessh}
     return render(request, "dashboard_web/dashboard.html", context )
+
+
+def ftp(request):
+    return render(request, "dashboard_web/ftp_overview.html")
+
+
+def endlessh(request):
+    return render(request, "dashboard_web/endlessh_overview.html")
