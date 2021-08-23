815b0673dcb758842168bb07b5ca3274c92f12f7
0xUnicorn <17219001+0xUnicorn@users.noreply.github.com>
2021-08-23 14:25:30 +0100

    Modified base.html to have custom-styles and custom-scripts blocks, for overwriting in specific pages. Added pagination table to ftp_overview.

--- dashboard_web/urls.py
+++ dashboard_web/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('test', views.test, name='test'),
     path('ftp', views.ftp, name='ftp_overview'),
     path('endlessh', views.endlessh, name='endlessh_overview')
 ]
--- dashboard_web/views.py
+++ dashboard_web/views.py
@@ -9,12 +9,17 @@ def index(request):
     ftp_logins = FtpLogin.get_10_latest()
     endlessh = Endlessh.get_10_latest()
     context = {'ftplogins' : ftp_logins, 'endlessh': endlessh}
-    return render(request, "dashboard_web/index.html", context )
+    return render(request, "dashboard_web/index.html", context)
 
 
 def ftp(request):
-    return render(request, "dashboard_web/ftp_overview.html")
+    ftp_logins = FtpLogin.get_all()
+    context = {'ftplogins': ftp_logins}
+    return render(request, "dashboard_web/ftp_overview.html", context)
 
 
 def endlessh(request):
     return render(request, "dashboard_web/endlessh_overview.html")
+
+def test(request):
+    return render(request, "dashboard_web/test.html")
